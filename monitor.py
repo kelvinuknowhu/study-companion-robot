09c2635e263391bbf49b223897b1dec7e639b53a
Bang <hb398@cornell.edu>
2017-11-25 14:57:00 -0500

    Initial logging capability implemented. Updates the README file

--- monitor.py
+++ monitor.py
@@ -11,64 +11,125 @@ from logger import getLogger
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 logger = getLogger('monitor')
 
+FACIAL_EXPRESSIONS = ['attention','browFurrow','browRaise','cheekRaise','chinRaise','dimpler','eyeClosure','eyeWiden','innerBrowRaise',
+                      'jawDrop','lidTighten','lipCornerDepressor','lipPress','lipPucker','lipSuck','mouthOpen','noseWrinkle','smile','smirk','upperLipRaise']
 
-#FACIAL_EXPRESSION_SCREEN="facial_expression_server_screen"
-#KEYLOGGER_SCREEN="key_logger_screen"
+WEBSITES = ['facebook','youtube','instagram','twitter','reddit','buzzfeed','tumblr','pinterest','news','blog','board','game']
 
-#def kill_screens():
-#    try:
-#        subprocess.check_output("killall SCREEN", shell=True)
-#    except subprocess.CalledProcessError as e:
-#        print(e.output)    
-#    
-#def run_command_on_screen(command):
-#    subprocess.run("screen -dm {0}".format(command), shell=True, check=True)
-#    
-#def open_new_screen(name,command=None):
-#    try:
-#        #subprocess.check_output("killall SCREEN", shell=True)
-#        subprocess.run("screen -S {0}".format(name), shell=True, check=True)
-#        
-#    except subprocess.CalledProcessError as e:
-#        print(e.output)            
-    
-#    subprocess.run("screen -S {0}".format(name), shell=True, check=True)
-#    if command is not None:
-#        run_command_on_screen(command)
+PORT = 8899
 
     
 class Monitor():
     
-    def __init__(self):
-        pass
-#        kill_screens()
-    
+    def __init__(self, saveFile = None):
+        if saveFile is None:
+            self.saveFile = os.path.join(DIR_PATH,"data","test.txt")
+        else:
+            self.saveFile = os.path.join(DIR_PATH,"data",saveFile)
+            
+        try:
+            with open(self.saveFile, 'rb') as _:
+                pass
+        except FileNotFoundError:
+            print("Created new file at: {0}".format(self.saveFile))
+            # Create new file
+            open(self.saveFile, 'w').close()                
+            
+        header = []
+        for key in FACIAL_EXPRESSIONS:
+            header.append(key)
+            
+        for key in WEBSITES:
+            header.append("active_{0}".format(key))
+            
+        header.append("active_other")
+        
+        for key in WEBSITES:
+            header.append("open_{0}".format(key))
+            
+        with open(self.saveFile, 'w') as f:
+            out = [str(x) for x in header]
+            f.write(",".join(out))
+            f.write('\n')  
+        
+        self.last_facial_expression_stored = {}
+        for key in FACIAL_EXPRESSIONS:
+            self.last_facial_expression_stored[key] = 0   
+
+                
     def run(self):
         # Assumes that the keylogger, the web server is running
-        timer = RepeatedTimer(20, logData)
+        timer = RepeatedTimer(5, self.logData)
         # start timer
         timer.start()
         
     def logData(self):
+        data = []
+        ind = 0
         
-        request_facial_expression()
-        read_facial_expression()
+        self.request_facial_expression()
+        facial_expression = self.read_facial_expression()
         
-        write_open_windows()
-        read_open_windows()
+        self.write_open_windows()
+        active_window, open_windows = self.read_open_windows()
         
-        
-#    def run_keylogger_process(self):
-#        open_new_screen(KEYLOGGER_SCREEN)
-#        run_command_on_screen("keylogger")
-#        
-#    def run_facial_expression_monitor_server(self):
-#        open_new_screen(FACIAL_EXPRESSION_SCREEN)
-#        server_file_path = os.path.join(DIR_PATH,"web","server.py")
-#        run_command_on_screen("python {0}".format(server_file_path))
+            
+        allZero = True
+        for key in facial_expression:
+            if not facial_expression[key] == 0:
+                allZero = False
+
+        if allZero:
+            for key in FACIAL_EXPRESSIONS:
+                facial_expression[key] = self.last_facial_expression_stored[key]                    
+        else:
+            for key in FACIAL_EXPRESSIONS:
+                self.last_facial_expression_stored[key] = facial_expression[key]        
+                
+                
+        for key in FACIAL_EXPRESSIONS:
+            data.append(facial_expression[key])
+
+                
+        if active_window:
+            foundMatch = False
+            for key in WEBSITES:
+                if key in active_window.lower():
+                    data.append(1)
+                    foundMatch = True
+                else:
+                    data.append(0)
+                    
+            if foundMatch:
+                data.append(0)
+            else:
+                data.append(1)
+        else:
+            for key in WEBSITES:
+                data.append(0)
+            data.append(0)
+            
+        if open_windows:
+            for key in WEBSITES:
+                counter = 0
+                for window in open_windows:
+                    if key in window.lower():
+                        counter += 1
+                data.append(counter)
+        else:
+            for key in WEBSITES:
+                data.append(0)
+
+        with open(self.saveFile,'a') as f:
+            out = [str(x) for x in data]
+            f.write(",".join(out))
+            f.write('\n')
+            print("Logged data with {0} features".format(len(data)))
+            print(",".join(out))
+
     
     def request_facial_expression(self):
-        requests.post('http://localhost:8889/get-facial-expression', data = {'key':'value'})
+        requests.post('http://localhost:{0}/get-facial-expression'.format(PORT), data = {'key':'value'})
         
     def read_facial_expression(self):
         data_file = os.path.join(DIR_PATH,"web","facial_expressions.txt")        
@@ -84,12 +145,18 @@ class Monitor():
                         pass
                     else:
                         lineSplit = line.split(":")
-                        facialExpressions[lineSplit[0]]=lineSplit[1]
+                        number = float(lineSplit[1].strip())
+                        facialExpressions[lineSplit[0]]=number
             os.remove(data_file)
+            return facialExpressions
 
         except OSError as e:
-            print('Facial expression not recognized yet')
+            print('Facial expression not recognized')
             print(str(e))
+            facialExpressions = {}
+            for exp in FACIAL_EXPRESSIONS:
+                facialExpressions[exp] = 0            
+            return facialExpressions            
                         
     def write_open_windows(self):
         script_path = os.path.join(DIR_PATH,"applescript","windows_monitor.scpt")
@@ -128,10 +195,14 @@ class Monitor():
                             lineSplit = line.split(",")
                             open_applications = lineSplit
             os.remove(data_file)
+            
+            return active_tab_title, open_tab_title
 
         except Error as e:
             print('Exe in reading the window monitor file')
             print(str(e))
+            
+            return None, None
             
             
 def waitForFileGeneration(filePath,trial=3,interval=1):
@@ -175,7 +246,6 @@ class RepeatedTimer(object):
 
 def main():
     pass
-    
 #    monitor = Monitor(server)
 #
 #    timer = RepeatedTimer(30, monitor_open_windows)
