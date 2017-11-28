deb00099bec28ee8253389ce1182f0f1fbdfcf5a
Kelvin <kelvin@energo.cn>
2017-11-28 15:49:20 -0500

    finished socket

--- transfer_prediction_result.py
+++ transfer_prediction_result.py
@@ -1,6 +1,14 @@
-
+import socket
 
 if __name__ == '__main__':
-     
-    result = "Distracted"
-    # Socket connection to the robot manipulation script
+    result = 'Distracted'
+
+    # Provides socket connection for the robot manipulation script
+    s = socket.socket()
+    ip = '127.0.0.1'
+    port = 20000
+    s.bind(ip, port)
+    s.listen()
+
+    c, addr = s.accept()
+    c.send(result)
